b85af54f34db8d33102fa66db50fd6fbfadcad05
Danyil-SY <41541307+Danyil-SY@users.noreply.github.com>
2024-01-28 21:06:45 +0200

    Create Regular Ball Super Ball.py

---
+++ python/8kyu/Regular Ball Super Ball.py
@@ -0,0 +1,15 @@
+# see description here: 
+# https://www.codewars.com/kata/53f0f358b9cb376eca001079/train/python
+
+# Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+
+# If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+
+# ball1 = Ball()
+# ball2 = Ball("super")
+# ball1.ball_type  #=> "regular"
+# ball2.ball_type  #=> "super"
+
+class Ball(object):
+    def __init__(self, ball_type='regular'):
+        self.ball_type = ball_type
