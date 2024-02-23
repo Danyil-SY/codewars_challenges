7d9e3f10f4fab603f851d5c285d539d0cadcc69b
Danyil-SY <41541307+Danyil-SY@users.noreply.github.com>
2024-02-23 23:40:09 +0200

    Create Sort arrays - 1.py

---
+++ python/7kyu/Sort arrays - 1.py
@@ -0,0 +1,6 @@
+# https://www.codewars.com/kata/51f41b98e8f176e70d0002a8/train/python
+
+# Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+def sortme(names):
+    return sorted(names)
