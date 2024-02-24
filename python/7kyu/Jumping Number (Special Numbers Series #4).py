f58940a679fb418a29146c6097a12cbf45f75f2b
Danyil-SY <41541307+Danyil-SY@users.noreply.github.com>
2024-02-24 16:02:42 +0200

    Create Jumping Number (Special Numbers Series #4).py

---
+++ python/7kyu/Jumping Number (Special Numbers Series #4).py
@@ -0,0 +1,48 @@
+# https://www.codewars.com/kata/5a54e796b3bfa8932c0000ed/train/python
+
+# Definition
+# Jumping number is the number that All adjacent digits in it differ by 1.
+
+# Task
+# Given a number, Find if it is Jumping or not .
+
+# Warm-up (Highly recommended)
+# Playing With Numbers Series
+# Notes
+# Number passed is always Positive .
+
+# Return the result as String .
+
+# The difference between ‘9’ and ‘0’ is not considered as 1 .
+
+# All single digit numbers are considered as Jumping numbers.
+
+# Input >> Output Examples
+# jumpingNumber(9) ==> return "Jumping!!"
+# Explanation:
+# It's single-digit number
+# jumpingNumber(79) ==> return "Not!!"
+# Explanation:
+# Adjacent digits don't differ by 1
+# jumpingNumber(23) ==> return "Jumping!!"
+# Explanation:
+# Adjacent digits differ by 1
+# jumpingNumber(556847) ==> return "Not!!"
+# Explanation:
+# Adjacent digits don't differ by 1
+# jumpingNumber(4343456) ==> return "Jumping!!"
+# Explanation:
+# Adjacent digits differ by 1
+# jumpingNumber(89098) ==> return "Not!!"
+# Explanation:
+# Adjacent digits don't differ by 1
+# jumpingNumber(32) ==> return "Jumping!!"
+# Explanation:
+# Adjacent digits differ by 1
+
+def jumping_number(number):
+    number = list(str(number))
+    for i in range(0, len(number)-1):
+        if abs(int(number[i+1])-int(number[i])) != 1:
+            return 'Not!!'
+    return 'Jumping!!'
