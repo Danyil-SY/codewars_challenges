5275caadd54c19adfd4f667ef33a0b3b88b114ee
Danyil-SY <41541307+Danyil-SY@users.noreply.github.com>
2024-04-04 23:54:56 +0300

    Create Sort by Last Char.py

---
+++ python/7kyu/Sort by Last Char.py
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/57eba158e8ca2c8aba0002a0/train/python
+
+# Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+# If two words have the same last letter, the returned array should show them in the order they appeared in the given string.
+
+# All inputs will be valid.
+
+def last(s):
+    return sorted(s.split(), key=lambda word: (word[-1], s.index(word)))
