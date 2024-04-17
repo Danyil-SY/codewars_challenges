979aaa5cf1b45650efe8f9c3f910ed2867dfde11
Danyil-SY <41541307+Danyil-SY@users.noreply.github.com>
2024-04-17 23:41:43 +0300

    Create Building Strings From a Hash.py

---
+++ python/7kyu/Building Strings From a Hash.py
@@ -0,0 +1,12 @@
+# https://www.codewars.com/kata/51c7d8268a35b6b8b40002f2/train/python
+
+# Complete the solution so that it takes the object (JavaScript/CoffeeScript) or hash (ruby) passed in and generates a human readable string from its key/value pairs.
+
+# The format should be "KEY = VALUE". Each key/value pair should be separated by a comma except for the last pair.
+
+# Example:
+
+# solution({"a": 1, "b": '2'}) # should return "a = 1,b = 2"
+
+def solution(pairs):
+    return ','.join(f'{key} = {val}' for key, val in pairs.items())
