670f5dfcd57a6d14830b74b688ffbd6498416fb2
Danyil-SY <41541307+Danyil-SY@users.noreply.github.com>
2024-04-24 23:47:40 +0300

    Create Find min and max.py

---
+++ python/7kyu/Find min and max.py
@@ -0,0 +1,6 @@
+# https://www.codewars.com/kata/57a1ae8c7cb1f31e4e000130/train/python
+
+# Implement a function that returns the minimal and the maximal value of a list (in this order).
+
+def get_min_max(seq): 
+    return (min(seq), max(seq))
