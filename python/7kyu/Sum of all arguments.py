c83ef932691f1a8c2a0acce08aaaef18b4e4a174
Danyil-SY <41541307+Danyil-SY@users.noreply.github.com>
2024-04-29 23:51:41 +0300

    Create Sum of all arguments.py

---
+++ python/7kyu/Sum of all arguments.py
@@ -0,0 +1,12 @@
+# https://www.codewars.com/kata/540c33513b6532cd58000259/train/python
+
+# Write a function that finds the sum of all its arguments.
+
+# eg:
+
+# sum_args(1, 2, 3) # => 6
+# sum_args(8, 2) # => 10
+# sum_args(1, 2, 3, 4, 5) # => 15
+
+def sum_args(*args):
+    return sum(args)
