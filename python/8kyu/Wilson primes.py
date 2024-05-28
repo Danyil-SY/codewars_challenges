7ae7d3f1a6861f5dfc33c9aa41f7cf35044fb50a
Danyil-SY <41541307+Danyil-SY@users.noreply.github.com>
2024-05-28 22:31:58 +0300

    Create Wilson primes.py

---
+++ python/8kyu/Wilson primes.py
@@ -0,0 +1,17 @@
+# https://www.codewars.com/kata/55dc4520094bbaf50e0000cb/train/python
+
+# Wilson primes satisfy the following condition. Let P represent a prime number.
+
+# Then,
+
+# ((P-1)! + 1) / (P * P)
+# should give a whole number.
+
+# Your task is to create a function that returns true if the given number is a Wilson prime.
+
+import math
+
+def am_i_wilson(n):
+    if n <= 2:
+        return False
+    return ((math.factorial(n - 1) + 1) % (n ** 2)) == 0
