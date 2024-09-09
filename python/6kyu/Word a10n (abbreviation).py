19c0ddc5d7b10ac09903abc9087b3c8129f84b55
Danyil-SY <41541307+Danyil-SY@users.noreply.github.com>
2024-09-09 23:45:56 +0300

    Create Word a10n (abbreviation).py

---
+++ python/6kyu/Word a10n (abbreviation).py
@@ -0,0 +1,33 @@
+# https://www.codewars.com/kata/5375f921003bf62192000746/train/python
+
+# The word i18n is a common abbreviation of internationalization in the developer community, used instead of typing the whole word and trying to spell it correctly. Similarly, a11y is an abbreviation of accessibility.
+
+# Write a function that takes a string and turns any and all "words" (see below) within that string of length 4 or greater into an abbreviation, following these rules:
+
+# A "word" is a sequence of alphabetical characters. By this definition, any other character like a space or hyphen (eg. "elephant-ride") will split up a series of letters into two words (eg. "elephant" and "ride").
+# The abbreviated version of the word should have the first letter, then the number of removed characters, then the last letter (eg. "elephant ride" => "e6t r2e").
+# Example
+# abbreviate("elephant-rides are really fun!")
+# //          ^^^^^^^^*^^^^^*^^^*^^^^^^*^^^*
+# // words (^):   "elephant" "rides" "are" "really" "fun"
+# //                123456     123     1     1234     1
+# // ignore short words:               X              X
+
+# // abbreviate:    "e6t"     "r3s"  "are"  "r4y"   "fun"
+# // all non-word characters (*) remain in place
+# //                     "-"      " "    " "     " "     "!"
+# === "e6t-r3s are r4y fun!"
+
+import re
+
+def abbreviate(string):
+    def abbreviate_word(word):
+        if len(word) < 4:
+            return word
+        return f"{word[0]}{len(word) - 2}{word[-1]}"
+    
+    words = re.split(r'([^a-zA-Z]+)', string)
+    
+    result = [abbreviate_word(word) if word.isalpha() else word for word in words]
+    
+    return ''.join(result)
