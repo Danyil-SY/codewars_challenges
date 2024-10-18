304cb4ee2f66592178e65f882fc1c60e4152e44b
Danyil-SY <41541307+Danyil-SY@users.noreply.github.com>
2024-10-18 22:42:06 +0300

    Create Find all occurrences of an element in an array.py

---
+++ python/7kyu/Find all occurrences of an element in an array.py
@@ -0,0 +1,15 @@
+# https://www.codewars.com/kata/59a9919107157a45220000e1/train/python
+
+# Given an array (a list in Python) of integers and an integer n, find all occurrences of n in the given array and return another array containing all the index positions of n in the given array.
+
+# If n is not in the given array, return an empty array [].
+
+# Assume that n and all values in the given array will always be integers.
+
+# Example:
+
+# find_all([6, 9, 3, 4, 3, 82, 11], 3)
+# > [2, 4]
+
+def find_all(array, n):
+    return [i for i, num in enumerate(array) if num == n]
