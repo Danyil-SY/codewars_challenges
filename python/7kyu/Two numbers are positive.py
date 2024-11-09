0b6217eed6389d0606e40b79b4e9d5399b00cb41
Danyil-SY <41541307+Danyil-SY@users.noreply.github.com>
2024-11-09 23:36:00 +0200

    Create Two numbers are positive.py

---
+++ python/7kyu/Two numbers are positive.py
@@ -0,0 +1,15 @@
+# https://www.codewars.com/kata/602db3215c22df000e8544f0/train/python
+
+# Task:
+# Your job is to write a function, which takes three integers a, b, and c as arguments, and returns True if exactly two of the three integers are positive numbers (greater than zero), and False - otherwise.
+
+# Examples:
+# two_are_positive(2, 4, -3) == True
+# two_are_positive(-4, 6, 8) == True
+# two_are_positive(4, -6, 9) == True
+# two_are_positive(-4, 6, 0) == False
+# two_are_positive(4, 6, 10) == False
+# two_are_positive(-14, -3, -4) == False
+
+def two_are_positive(a, b, c):
+    return sum(1 for x in [a, b, c] if x > 0) == 2
